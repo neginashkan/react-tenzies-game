df4c8076e61fb826237b647ce2c2432e69eb3f72
negnq <117580177+neginashkan@users.noreply.github.com>
2023-03-28 07:14:15 +0430

    add Main component , and style it

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import './CSS/styles.css'
-
+import Main from "./components/Main"
 function App() {
 
   return (
     <div className="App">
+      <Main/>
     </div>
   )
 }
